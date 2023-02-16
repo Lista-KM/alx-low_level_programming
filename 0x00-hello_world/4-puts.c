331c3b9a1a06787f5adb5efcda922b6ddcfaf935
Lista-KM <mngeikerubo@gmail.com>
2023-02-16 06:10:55 -0800

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - I am really trying
+ * Return: 0 (This means the progarm is a success_)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
