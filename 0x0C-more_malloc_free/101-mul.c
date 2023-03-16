6ae3a40fdf77f1bb835b938a4dd4bc81d4eae1a1
Lista-KM <mngeikerubo@gmail.com>
2023-03-16 07:03:23 -0700

    Second last

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,83 +1,83 @@
-#include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
-#define ERR_MSG "Error"
+#include <stdlib.h>
+#include <ctype.h>
+
 /**
- * is_digit - checks if a string contains a non-digit char
- * @s: string to be evaluated
+ * multiply - multiplies two integers
+ * @num1: first integer
+ * @num2: second integer
  *
- * Return: 0 if a non-digit is found, 1 otherwise
+ * Return: product of num1 and num2
  */
-int is_digit(char *s)
+int multiply(int num1, int num2)
 {
-int i = 0;
-while (s[i])	
-{
-if (s[i] < '0' || s[i] > '9')
-return (0);
-i++;
-return (1);
-}
-/**
- * _strlen - returns the length of a string
- * @s: string to evaluate
- *
- * Return: the length of the string
- */
-int _strlen(char *s)
-{
-int i = 0;
-while (s[i] != '\0')
-{
-i++;
-}
-return (i);
+	return (num1 * num2);
 }
 
-void errors(void)
+/**
+ * is_valid_input - checks if input numbers are valid
+ * @num1: first number as string
+ * @num2: second number as string
+ *
+ * Return: 1 if valid, 0 otherwise
+ */
+int is_valid_input(char *num1, char *num2)
 {
-printf("Error\n");
-exit(98);
+	while (*num1 != '\0')
+	{
+		if (!isdigit(*num1))
+		{
+			return (0);
+		}
+		num1++;
+	}
+
+	while (*num2 != '\0')
+	{
+		if (!isdigit(*num2))
+		{
+			return (0);
+		}
+		num2++;
+	}
+
+	return (1);
 }
 
+/**
+ * print_error - prints error message and exits program with status 98
+ */
+void print_error(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * main - entry point, multiplies two positive integers
+ * @argc: number of command-line arguments
+ * @argv: array of command-line arguments
+ *
+ * Return: 0 on success, 98 on failure
+ */
 int main(int argc, char *argv[])
 {
-char *s1, *s2;
-int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
-s1 = argv[1], s2 = argv[2];
-if (argc != 3 || !is_digit(s1) || !is_digit(s2))	
-errors);
-len1 = _strlen(s1);
-len2 = _strlen(s2);
-len = len1 + len2 + 1;
-result = malloc(sizeof(int) * len);
-if (!result)
-return (1);
-for (i = 0; i <= len1 + len2; i++)
-result[i] = 0;
-for (len1 = len1 - 1; len1 >= 0; len1--)
+if (argc != 3)
 {
-digit1 = s1[len1] - '0';
-carry = 0;
-for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+print_error();
+}
+
+char *num1 = argv[1];
+char *num2 = argv[2];
+
+if (!is_valid_input(num1, num2))
 {
-digit2 = s2[len2] - '0';
-carry += result[len1 + len2 + 1] + (digit1 * digit2);
-result[len1 + len2 + 1] = carry % 10;
-carry /= 10;
+print_error();
 }
-if (carry > 0)
-result[len1 + len2 + 1] += carry;
-}
-for (i = 0; i < len - 1; i++)
-if (result[i])
-a = 1;
-if (a)
-_putchar(result[i] + '0');
-}
-if (!a)
-_putchar('0');
-_putchar('\n');
-free(result);
+
+int result = multiply(atoi(num1), atoi(num2));
+printf("%d\n", result);
+
 return (0);
 }
+
