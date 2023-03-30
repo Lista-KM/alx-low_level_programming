e7cde306d663b9045c03fe663b1499faadd57514
Lista-KM <mngeikerubo@gmail.com>
2023-03-30 14:10:19 -0700

    Linked Lists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,10 +1,9 @@
-
-       global    main
+      global    main
           extern    printf
 main:
 	  mov   edi, format
 	  xor   eax, eax
 	  call  printf
- mov 	eax,
-ret
+	  mov 	eax, 0
+	  ret
 format: db `Hello, Holberton\n`,0
