ad26f02ec112011d14a50aa360d0874840d1070f
Lista-KM <mngeikerubo@gmail.com>
2023-04-03 08:10:18 -0700

    Correction

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,3 @@
-#ifndef LISTS_H
-#define LISTS_H
-
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -14,8 +10,8 @@
  */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s*next;
+        int n;
+        struct listint_s*next;
 }
 listint_t;
 
